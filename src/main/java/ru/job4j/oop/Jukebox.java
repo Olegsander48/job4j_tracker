9499a0011b814c6dc5f929310a356ba4fa7ccc39
Olegsander48 <olegsander2001@gmail.com>
2022-03-09 13:08:27 +0300

    1.3. Вызов метода с аргументами. [#166086 #273014]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -3,16 +3,16 @@ package ru.job4j.oop;
 public class Jukebox {
     public void music(int position) {
         if (position == 1) {
-            System.out.println("Пусть бегут неуклюже\n"
-                    + "Пешеходы по лужам,\n"
-                    + "А вода по асфальту рекой.\n"
-                    + "И неясно прохожим\n"
-                    + "В этот день непогожий,\n"
-                    + "Почему я веселый такой?");
+            System.out.println("Пусть бегут неуклюже" + System.lineSeparator()
+                    + "Пешеходы по лужам," + System.lineSeparator()
+                    + "А вода по асфальту рекой." + System.lineSeparator()
+                    + "И неясно прохожим" + System.lineSeparator()
+                    + "В этот день непогожий," + System.lineSeparator()
+                    + "Почему я веселый такой?" + System.lineSeparator());
         } else if (position == 2) {
-            System.out.println("Спокойной ночи, господа, спокойной ночи,\n"
-                    + "Ваш день прошел, а ночь колдует и пророчит.\n"
-                    + "Ночные сказки, отголоски дней минувших\n"
+            System.out.println("Спокойной ночи, господа, спокойной ночи," + System.lineSeparator()
+                    + "Ваш день прошел, а ночь колдует и пророчит." + System.lineSeparator()
+                    + "Ночные сказки, отголоски дней минувших" + System.lineSeparator()
                     + "Спокойной ночи, добрых снов для всех уснувших.");
         } else {
             System.out.println("Песня не найдена");
