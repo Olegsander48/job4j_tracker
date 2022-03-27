9331d02b6463b0c567e846ed10d000d15a37f8de
Olegsander48 <olegsander2001@gmail.com>
2022-03-27 09:58:21 +0300

    0.4. Пользовательские исключения. [#219367 #280458]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,26 +1,7 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
-                rsl = i;
-                break;
-            }
-        }
-        if (rsl == -1) {
-            throw new ElementNotFoundException();
-        } /*Search element not found in string array*/
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        String[] value = {"a", "bcd", "gef"};
-        try {
-            ElementNotFoundException.indexOf(value, "f");
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
+    public ElementNotFoundException(String message) {
+        super(message);
     }
 }
