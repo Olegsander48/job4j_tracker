2696bc464aa5871444bb609ae5d67000bbb2e1ca
Olegsander48 <olegsander2001@gmail.com>
2022-04-23 09:07:49 +0300

    2. Что такое JavaDoc [#313879 #292455]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Класс описывает модель простейшей банковской системы
+ * Класс описывает работу с банковскими счетами
  * по принципу FIFO (first in - first out)
  * @author PRIGODICH ALEKSANDR
  * @version 1.0
