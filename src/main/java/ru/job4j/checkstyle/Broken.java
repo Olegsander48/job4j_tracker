d00729292b6f357455ca14280cf9feb16c3f0e0f
Olegsander48 <prigodichaleks@gmail.com>
2024-12-10 12:42:33 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private final int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    Broken() {
+    }
+
+    void echo() {
+    }
+
+    void inform(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+
+    }
+
+    void method(int a, int b, int c, int d) {
+    }
+}
