5eba624183a258b1e5e3e5c3f6c80e5a7865734a
Olegsander48 <prigodichaleks@gmail.com>
2024-12-12 19:42:18 +0300

    12. Enum  [#504960]

---
+++ src/main/java/ru/job4j/enumeration/CarService.java
@@ -0,0 +1,10 @@
+package ru.job4j.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.IN_WORK;
+        System.out.println(toyota);
+        System.out.println(volvo);
+    }
+}
---
+++ src/main/java/ru/job4j/enumeration/Status.java
@@ -0,0 +1,56 @@
+package ru.job4j.enumeration;
+
+public enum Status {
+    ACCEPTED("Принят") {
+        private String message = "Автомобиль принят на СТО";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+
+    },
+    IN_WORK("В работе") {
+        private String message = "Автомобиль в работе";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    WAITING("Ожидание") {
+        private String message = "Автомобиль ожидает запчасти";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    },
+    FINISHED("Работы завершены") {
+        private String message = "Все работы завершены";
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    };
+
+    private String info;
+
+    Status(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public abstract String getMessage();
+
+    public static void main(String[] args) {
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
+        }
+    }
+}
