76fcaef89b2cd4e696da890075a85638db5bdb8d
Olegsander48 <prigodichaleks@gmail.com>
2024-12-12 19:02:57 +0300

    10. toString [#310018 #274795]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,7 @@ public class StartUI {
         LocalDateTime created = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(created.format(formatter));
+
+        System.out.println(item);
     }
 }
