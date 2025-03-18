e2c50070283a6a7edd849de921faa40e05af932a
Aleksandr Prigodich <64789713+Olegsander48@users.noreply.github.com>
2025-03-18 09:31:01 +0300

    3. Liquibase. Интеграционные тесты. [#504894]

--- src/main/resources/db/scripts/003_ddl_alter_table_items_drop_checked_column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE items
-    DROP COLUMN IF EXISTS checked;
---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,110 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SqlTrackerTest {
+    private static Connection connection;
+
+    @BeforeAll
+    public static void initConnection() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("db/liquibase_test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterAll
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @AfterEach
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.findById(item.getId())).isEqualTo(item);
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceITAndFindByGeneratedIdThenMustBeTheReplaced() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item replaceItem = new Item("Item24");
+        tracker.replace(item.getId(), replaceItem);
+        assertThat(tracker.findById(item.getId())).isEqualTo(replaceItem);
+    }
+
+    @Test
+    public void whenSaveItemAndDeleteItemThemMustBeDeleted() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        tracker.delete(item.getId());
+        assertThat(tracker.findById(item.getId())).isEqualTo(null);
+    }
+
+    @Test
+    public void whenSaveFiveItemsAndFindThemAllThemMustBeFiveItems() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item1 = new Item("item1");
+        Item item2 = new Item("item2");
+        Item item3 = new Item("item3");
+        Item item4 = new Item("item4");
+        Item item5 = new Item("item5");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.add(item5);
+        List<Item> result = tracker.findAll();
+        assertThat(result)
+                .isNotEmpty()
+                .hasSize(5)
+                .contains(item1, item2, item3, item4, item5);
+    }
+
+    @Test
+    public void whenSaveTwoItemsWithSameNameAndFindByNameThemMustBeTwoItems() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item1 = new Item("item");
+        Item item2 = new Item("item");
+        tracker.add(item1);
+        tracker.add(item2);
+        List<Item> result = tracker.findByName("item");
+        assertThat(result)
+                .isNotEmpty()
+                .hasSize(2)
+                .contains(item1, item2);
+    }
+}
