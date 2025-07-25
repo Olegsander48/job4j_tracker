e8f6db0cadb412b2cf61de8b85de6f287aca9cd2
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-07-25 13:36:56 +0300

    3. HbmTracker [#287705 #549145]

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -17,67 +17,119 @@ public class HbmTracker implements Store, AutoCloseable {
     @Override
     public Item add(Item item) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
         return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
+        Item itemFromDb;
         Session session = sf.openSession();
-        session.beginTransaction();
-        Item itemFromDb = session.get(Item.class, id);
-        if (itemFromDb != null) {
-            itemFromDb.setName(item.getName());
-            session.update(itemFromDb);
+        try {
+            session.beginTransaction();
+            itemFromDb = session.get(Item.class, id);
+            if (itemFromDb != null) {
+                itemFromDb.setName(item.getName());
+                session.update(itemFromDb);
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
         }
-        session.getTransaction().commit();
-        session.close();
         return itemFromDb != null;
     }
 
     @Override
     public void delete(int id) {
         Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        if (item != null) {
-            session.delete(item);
+        try {
+            session.beginTransaction();
+            Item item = session.get(Item.class, id);
+            if (item != null) {
+                session.delete(item);
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
         }
-        session.getTransaction().commit();
-        session.close();
     }
 
     @Override
     public List<Item> findAll() {
+        List<Item> result;
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.tracker.Item", Item.class).list();
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            result = session.createQuery("from ru.job4j.tracker.Item", Item.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
         return result;
     }
 
     @Override
     public List<Item> findByName(String key) {
+        List<Item> result;
         Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from ru.job4j.tracker.Item where name = :name", Item.class)
-                .setParameter("name", key).list();
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            result = session.createQuery("from ru.job4j.tracker.Item where name = :name", Item.class)
+                    .setParameter("name", key).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
         return result;
     }
 
     @Override
     public Item findById(int id) {
+        Item item;
         Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            item = session.get(Item.class, id);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (session.getTransaction() != null && session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
         return item;
     }
 
